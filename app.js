4464c471b4a55bc248924db5c776c2ab6574f5b4
TheRealKamikaze <tarun.lohana@somaiya.edu>
2020-05-20 22:33:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+      app = express();
+      bodyParser = require('body-parser');
+      mongoose   = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+mongoose.connect('mongodb+srv://tarun:QyDqfwsy7sulkeIo@cluster0-a0vpp.mongodb.net/minds2mentor?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}).catch((err)=>{
+  console.log(err);
+})
+
+app.post('/book',async (req,res)=>{
+  console.log(req.body);
+  res.json({
+    "status": "received"
+  })
+})
+
+app.listen(3000,()=>{
+  console.log('restro-booking!!!');
+})
