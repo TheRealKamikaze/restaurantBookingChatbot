c280d91508d9911a20b015a19a341a4091c29e54
TheRealKamikaze <tarun.lohana@somaiya.edu>
2020-05-21 19:15:57 +0530

    first commit

--- app.js
+++ app.js
@@ -10,82 +10,84 @@ mongoose.connect('mongodb+srv://tarun:QyDqfwsy7sulkeIo@cluster0-a0vpp.mongodb.ne
 })
 
 app.post('/book',async (req,res)=>{
-  console.log(req.body.queryResult);
-  console.log(req.body.queryResult.outputContexts)
-  if(req.body.queryResult.intent.displayName==='bookTable'){
-    // console.log('in')
-    // console.log(req.body.queryResult.outputContexts)
-    let date = req.body.queryResult.parameters.date;
-    let pos = req.body.queryResult.parameters.date.indexOf('T');
-    let saveDate = date.substring(0,pos);
-    let time = req.body.queryResult.parameters.time[0];
-    let startPos = time.indexOf('T')+1;
-    let endPos = time.indexOf('+');
-    let saveTime = time.substring(startPos,endPos);
-    let isAvailable = await booking.findOne({dateTime: saveDate + " "+ saveTime});
-    // console.log(isAvailable);
-    if(isAvailable===null){
+  try{
+    console.log(req.body.queryResult);
+    console.log(req.body.queryResult.outputContexts)
+    if(req.body.queryResult.intent.displayName==='bookTable'){
+      // console.log('in')
+      // console.log(req.body.queryResult.outputContexts)
+      let date = req.body.queryResult.parameters.date;
+      let pos = req.body.queryResult.parameters.date.indexOf('T');
+      let saveDate = date.substring(0,pos);
+      let time = req.body.queryResult.parameters.time[0];
+      let startPos = time.indexOf('T')+1;
+      let endPos = time.indexOf('+');
+      let saveTime = time.substring(startPos,endPos);
+      let isAvailable = await booking.findOne({dateTime: saveDate + " "+ saveTime});
+      // console.log(isAvailable);
+      if(isAvailable===null){
+        let response = {
+          "fulfillmentMessages": [
+            {
+              "text": {
+                "text": [
+                  "Booking available, confirm booking?"
+                ]
+              }
+            }
+          ]
+        }
+        res.json(response)
+      }else{
+        let response = {
+          "fulfillmentMessages": [
+            {
+              "text": {
+                "text": [
+                  "Booking not available, please pick a different time."
+                ]
+              }
+            }
+          ],
+          "outputContexts": req.body.queryResult.outputContexts
+        }
+        res.json(response)
+      }
+    }else if(req.body.queryResult.intent.displayName==='bookTable - yes'){
+      console.log(req.body.queryResult);
+      let contexts = req.body.queryResult.outputContexts;
+      let name = contexts[0].parameters.name;
+      let date = contexts[0].parameters.date;
+      let time = contexts[0].parameters.time[0];
+      let guests = contexts[0].parameters.number;
+      let pos = req.body.queryResult.parameters.date.indexOf('T');
+      let saveDate = date.substring(0,pos);
+      let startPos = time.indexOf('T')+1;
+      let endPos = time.indexOf('+');
+      let saveTime = time.substring(startPos,endPos);
+      let addBooking = {
+        name: name,
+        dateTime: saveDate + " " + saveTime,
+        guests: 3
+      }
+      let booked = await booking.create(addBooking);
+      console.log(booked)
       let response = {
         "fulfillmentMessages": [
           {
             "text": {
               "text": [
-                "Booking available, confirm booking?"
+                "Booking confirmed, have a nice day."
               ]
             }
           }
         ]
       }
       res.json(response)
-    }else{
-      let response = {
-        "fulfillmentMessages": [
-          {
-            "text": {
-              "text": [
-                "Booking not available, please pick a different time."
-              ]
-            }
-          }
-        ],
-        "outputContexts": req.body.queryResult.outputContexts
-      }
-      res.json(response)
     }
-  }else if(req.body.queryResult.intent.displayName==='bookTable - yes'){
-    console.log(req.body.queryResult);
-    let contexts = req.body.queryResult.outputContexts;
-    let name = contexts[0].parameters.name;
-    let date = contexts[0].parameters.date;
-    let time = contexts[0].parameters.time[0];
-    let guests = contexts[0].parameters.number;
-    let pos = req.body.queryResult.parameters.date.indexOf('T');
-    let saveDate = date.substring(0,pos);
-    let startPos = time.indexOf('T')+1;
-    let endPos = time.indexOf('+');
-    let saveTime = time.substring(startPos,endPos);
-    let addBooking = {
-      name: name,
-      dateTime: saveDate + " " + saveTime,
-      guests: 3
-    }
-    let booked = await booking.create(addBooking);
-    console.log(booked)
-    let response = {
-      "fulfillmentMessages": [
-        {
-          "text": {
-            "text": [
-              "Booking confirmed, have a nice day."
-            ]
-          }
-        }
-      ]
-    }
-    res.json(response)
+  }catch(err){
+    console.log(err);
   }
-}).catch((err)=>{
-  console.log(err);
 })
 
 app.get('/',(req,res)=>{
